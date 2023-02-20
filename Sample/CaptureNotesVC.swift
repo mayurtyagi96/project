dc8b34d2acf353aacf9de0a2109e0c1771dc950f
Mayur <mayurtyagi@unolo.com>
2023-02-20 21:55:38 +0530

    Initial Commit

---
+++ Sample/CaptureNotesVC.swift
@@ -0,0 +1,29 @@
+//
+//  CaptureNotes.swift
+//  Sample
+//
+//  Created by Mayur  on 20/02/23.
+//
+
+import UIKit
+
+class CaptureNotesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sample/LogInPageVC.swift
@@ -0,0 +1,31 @@
+//
+//  LogInPageVC.swift
+//  Sample
+//
+//  Created by Mayur  on 20/02/23.
+//
+
+import UIKit
+
+class LogInPageVC: UIViewController {
+   
+    @IBOutlet weak var logInLabel: UILabel!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logInBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func logInBtnPressed(_ sender: Any) {
+        let window = (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window
+        window?.rootViewController = self.storyboard?.instantiateViewController(withIdentifier: "TabBarVC")
+        
+    }
+    
+    
+
+}
